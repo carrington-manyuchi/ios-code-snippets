d4273bd358f78b50c86a9fbb365809cdc897095f
Carrington <Manyuchi>
2023-08-06 08:01:50 +0200

    navigating from SecondVC to ThirdVC using a Storyboard ID

--- UI_Navigation_Controllers/UI_Navigation_Controllers/RootViewController.swift
+++ UI_Navigation_Controllers/UI_Navigation_Controllers/RootViewController.swift
@@ -8,12 +8,8 @@
 import UIKit
 
 class RootViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
